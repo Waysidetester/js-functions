5cc6d2d36ad96e251726841b74bff6e01a670b03
WaysideTester <dylanmurray2119@gmail.com>
2018-08-25 11:22:04 -0500

    added numberAdder function

--- functions.js
+++ functions.js
@@ -21,3 +21,13 @@ const nomnom = (name,food) => {
 }
 
 console.log(nomnom('Dylan', bearSticks)); // Dylan devours bear stix
+
+//numberAdder must take number and add 3
+
+const numberAdder = (number) => {
+    return number+3;
+}
+
+console.log(numberAdder('Number:', 10));
+console.log(numberAdder('Number:', 9));
+console.log(numberAdder('Number:', 3));

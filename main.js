198b985a9012f8590742fda54bdd4a556254a966
WaysideTester <dylanmurray2119@gmail.com>
2018-08-25 10:17:12 -0500

    updated name

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ counter = counter + 1;
 
 console.log('counter: ' + counter);
 const greeting = document.getElementById('greeting');
-const fName = 'Slim';
-const lName = 'Shady';
+const fName = 'Dylan';
+const lName = 'Murray';
 greeting.innerHTML = `Hello my name is ${fName} ${lName}`;

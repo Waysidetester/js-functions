8c41e1a42d4af34fe5770b89f9a0aadb61444c50
WaysideTester <dylanmurray2119@gmail.com>
2018-08-25 10:11:01 -0500

    working on simple JavaScript

---
+++ main.js
@@ -0,0 +1,13 @@
+const  bananaDiv = document.getElementById('banana');
+bananaDiv.innerHTML = 'I\'m a banana';
+
+let counter = 0;
+counter += 1;
+counter ++;
+counter = counter + 1;
+
+console.log('counter: ' + counter);
+const greeting = document.getElementById('greeting');
+const fName = 'Slim';
+const lName = 'Shady';
+greeting.innerHTML = `Hello my name is ${fName} ${lName}`;
